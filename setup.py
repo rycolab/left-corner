72ceb780a8bed5b681a881a9014bd9a87b772719
Tim Vieira <tim.f.vieira@gmail.com>
2023-12-21 18:13:08 -0500

    ()

--- setup.py
+++ setup.py
@@ -2,8 +2,16 @@ from setuptools import setup
 
 setup(
     name='leftcorner',
+    version='1.0.1',
+    description=(
+        "Implementation of the generalized left-corner transformation (Opedal et al. 2023), "
+        "an efficient method for eliminating left recursion from context-free grammars, "
+        "and many other utilities for working with context-free grammars."
+    ),
+    project_url = 'https://github.com/rycolab/left-corner',
     install_requires = [
         'numpy',
+        'IPython',
         'networkx',
         'nltk',
         'svgling',    # nltk uses svgling to draw derivations
@@ -13,7 +21,14 @@ setup(
         'graphviz',   # for notebook visualization of left-recursion graph
         'path'
     ],
-    version='1.0',
+    authors = [
+        'Andreas Opedal',
+        'Eleftheria Tsipidi'
+        'Tiago Pimentel',
+        'Ryan Cotterell',
+        'Tim Vieira',
+    ],
+    readme=open('README.md').read(),
     scripts=[],
     packages=['leftcorner'],
 )
